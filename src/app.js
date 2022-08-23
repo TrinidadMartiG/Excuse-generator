46efaa3edebd045b7d9766da8a8e06d5551803b3
TrinidadMartiG <trinidad.martig@gmail.com>
2022-08-23 16:53:39 +0000

    added arrays, function, and button funciontalities

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let quien = [
+    "Mi compañer@",
+    "Will Smith",
+    "Mi conejo",
+    "El payaso IT",
+    "Un marciano",
+    "Rambo"
+  ];
+  let como = ["borro", "quito", "exploto", "rompio", "bofeteo", "modifico"];
+  let que = [
+    "mi aplicación",
+    "mi código",
+    "la modificación de la nueva branch",
+    "el commit",
+    "el link para el css"
+  ];
+  let cuando = [
+    "cuando venia para aca",
+    "ayer por la noche",
+    "mientras intentaba hacer git push",
+    "cuando entre al baño",
+    "cuando terminaba la app",
+    "mientras instalaba el boilerplate"
+  ];
+
+  let quienRandom = Math.floor(Math.random() * quien.length);
+  let comoRandom = Math.floor(Math.random() * como.length);
+  let queRandom = Math.floor(Math.random() * que.length);
+  let cuandoRandom = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[quienRandom] +
+    " " +
+    como[comoRandom] +
+    " " +
+    que[queRandom] +
+    " " +
+    cuando[cuandoRandom]
+  );
 };
